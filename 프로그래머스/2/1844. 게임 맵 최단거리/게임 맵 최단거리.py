fc641e5640deeef5823358539ac66213650b9a6d
Jang Yu Gyeong <104052659+yugyeong1@users.noreply.github.com>
2025-08-27 12:09:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,12 +3,14 @@ from collections import deque
 def solution(maps):
     result = bfs(0, 0, maps)
     return -1 if result == 1 else result
-
+    
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def bfs(x, y, maps):
-    n, m = len(maps), len(maps[0])  # 행, 열 크기
+    n = len(maps)
+    m = len(maps[0])
+    
     queue = deque()
     queue.append((x, y))
     
@@ -16,19 +18,19 @@ def bfs(x, y, maps):
         x, y = queue.popleft()
         
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
             
-            # 범위 벗어나면 continue
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
                 
-            # 벽이면 continue
             if maps[nx][ny] == 0:
                 continue
-            
-            # 처음 방문한 길이면 거리 갱신
+                
             if maps[nx][ny] == 1:
                 maps[nx][ny] = maps[x][y] + 1
                 queue.append((nx, ny))
                 
     return maps[n-1][m-1]
+        
+    
